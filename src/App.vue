11e01e2f7a8152beb47d47751b09307ed72a2af5
Roman Tsyupryk <cooprin@gmail.com>
2025-01-10 19:11:03 +0100

    final test

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ watch(
     document.title = newTitle || 'CRM for You (Cooprin)'; // Встановлюємо назву вкладки
   },
 );
-//for test
+
 // Для першого завантаження сторінки
 document.title = route.meta.title || 'CRM for You (Cooprin)';
 
