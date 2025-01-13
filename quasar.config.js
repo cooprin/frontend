35da1325ce993640daa1ff1e10a3420f0611bca2
Roman Tsyupryk <cooprin@gmail.com>
2025-01-13 15:30:20 +0100

    darkNEW

--- quasar.config.js
+++ quasar.config.js
@@ -81,7 +81,9 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: { dark: 'auto', storage: 'local' },
+      config: {
+        dark: localStorage.getItem('darkMode') === 'true',
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
