e3afb41c6f2ab5893776eaf3cd8892cca31584bd
Roman Tsyupryk <cooprin@gmail.com>
2025-01-15 11:47:27 +0100

    noENV2

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3000',
+  baseURL: process.env.API_URL || 'http://localhost:5000',
   headers: {
-    'Content-Type': 'application/json'
-  }
-});
+    'Content-Type': 'application/json',
+  },
+})
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
 // Додайте перехоплювачі для дебагу
-api.interceptors.request.use(request => {
+api.interceptors.request.use((request) => {
   console.log('Запит:', request)
   return request
 })
 
 api.interceptors.response.use(
-  response => {
+  (response) => {
     console.log('Успішна відповідь:', response)
     return response
   },
-  error => {
+  (error) => {
     console.error('Помилка відповіді:', error)
     return Promise.reject(error)
-  }
+  },
 )
 
-export { api };
+export { api }
