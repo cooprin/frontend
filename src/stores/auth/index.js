9f7931921cc27a0be727a1d0b7853506f340b701
Roman Tsyupryk <cooprin@gmail.com>
2025-01-17 11:03:03 +0100

    testafreC1

--- src/stores/auth/index.js
+++ src/stores/auth/index.js
@@ -3,6 +3,8 @@ import { useAuthActions } from './authActions'
 import { useAuthState } from './authState'
 
 export const useAuthStore = defineStore('auth', {
-  state: useAuthState, // Прибираємо виклик функції ()
-  actions: useAuthActions, // Прибираємо виклик функції ()
+  state: () => ({
+    ...useAuthState(),
+  }),
+  actions: useAuthActions(),
 })
