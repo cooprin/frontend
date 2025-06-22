60e9a9616625bcdd7b0b7412e4d92de070627092
cooprin <cooprin@gmail.com>
2025-06-22 16:33:02 +0200

    PSC2

---
+++ src/pages/portal/InvoicesPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <h1>My Invoices</h1>
+    <!-- Тут буде ваш контент для інвойсів -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'InvoicesPage',
+}
+</script>
