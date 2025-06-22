916cea3fa98328713f2bf5d9f86bfaf37a513a59
cooprin <cooprin@gmail.com>
2025-06-22 16:43:30 +0200

    PSC4

---
+++ src/pages/portal/TicketDetailsPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <h1>My TicketDetailsPage</h1>
+    <!-- Тут буде ваш контент для документів -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'TicketDetailsPage',
+}
+</script>
